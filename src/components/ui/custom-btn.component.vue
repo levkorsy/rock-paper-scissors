ac50f7d6f1f87bc8936d2f14414a6700db5ad556
lev_k <lev_k@malam-payroll.com>
2022-09-18 18:43:50 +0300

    added custom button

---
+++ src/components/ui/custom-btn.component.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <button class="custom-btn" :class="{ 'secondary': secondary  }" role="button" @click="emit('btn-click')">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = 
+  defineProps<{
+    secondary: boolean;
+  }>();
+  const emit = defineEmits(['btn-click'])
+</script>
+
+<style scoped>
+.custom-btn {
+  color: var(--rules-btn-color);
+  border: 1px solid var(--rules-btn-color);
+  border-radius: var(--global-border-radius);
+  width: 8rem;
+  height: 2.5rem;
+  cursor: pointer;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: var(--font-weight-bold);
+  box-shadow: var(--rules-btn-color) 0 1px 3px 0;
+  box-sizing: border-box;
+  transition: all 250ms;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: baseline;
+  position: relative;
+}
+
+.custom-btn:hover {
+  transform: translateY(-2px);
+}
+
+.custom-btn.secondary {
+  color: var(--rules-btn-color);
+  background: inherit;
+  border: 1px solid var(--rules-btn-color);
+}
+
+
+.custom-btn.secondary:hover,
+.custom-btn.secondary:focus {
+  border-color: rgba(0, 0, 0, 0.15);
+  box-shadow: var(--main-bg-gradient-1) 0 4px 12px;
+  background-color: var(--rules-btn-color);
+  color: var(--main-bg-gradient-1)
+}
+
+.custom-btn.secondary:active {
+  transform: translateY(0);
+}
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import HeaderComponent from "@/components/business/header/header.component.vue";
+import RulesComponent from "@/components/business/rules/rules.component.vue";
 </script>
 
 <template>
   <main class="main-container">
     <header-component/>
-    <div>dice</div>
-    <div>rules</div>
-    <div>rules popup</div>
+    <div style="flex: 2">dice</div>
+    <rules-component/>
   </main>
 </template>
 
